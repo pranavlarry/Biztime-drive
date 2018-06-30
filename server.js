e0f3d0dff2d492e2e446adccf7daa6822c62e9e2
Pranav <pranavlarry111@gmail.com>
2018-06-30 10:04:01 +0530

    biztime 2

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const flash    = require('connect-flash');
-//const morgan       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
 const session      = require('express-session');
@@ -14,22 +13,21 @@ const session      = require('express-session');
 
 var configDB = require('./config/database.js');
 
-// configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
 
-require('./config/passport')(passport); // pass passport for configuration
+mongoose.connect(configDB.url); 
 
-// set up our express application
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+require('./config/passport')(passport);
 
-app.set('view engine', 'ejs'); // set up ejs for templating
 
-// required for passport
-app.use(session({ secret: 'biztime' })); // session secret
+app.use(cookieParser());
+app.use(bodyParser()); 
+
+app.set('view engine', 'ejs'); 
+
+
+app.use(session({ secret: 'biztime' })); 
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(passport.session()); 
+app.use(flash()); 
 
-// routes ======================================================================
-require('./app/routes.js')(fs,multer,path,app,passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(fs,multer,path,app,passport); 
